8f0c96bf89dabf43813811afa982588a223dfaab
trebladev <2253714301@qq.com>
2023-07-21 18:39:49 +0800

    feat: set expandtab false, and add lua scripts for copilot

--- options.lua
+++ options.lua
@@ -7,6 +7,9 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    expandtab = false,
+    list = true,
+    listchars="trail:.",
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
--- plugins/core.lua
+++ plugins/core.lua
@@ -20,6 +20,7 @@ return {
       return opts
     end,
   },
+  { "github/copilot.vim" },
   -- You can disable default plugins as follows:
   -- { "max397574/better-escape.nvim", enabled = false },
   --
